7e503c3c68e3b32631e1ae59b81dba437a8f7fd3
Albert Garde <albertsgarde@gmail.com>
2025-12-05 15:40:31 +0100

    :memo: Improve documentation of buildings

--- rustorio/src/buildings.rs
+++ rustorio/src/buildings.rs
@@ -17,11 +17,11 @@ use crate::{
 
 /// The assembler is used for recipes that require two different inputs to produce an output.
 ///
-/// To use, first build the assembler using `Assembler::build`, providing the desired recipe and the required resources.
-/// Then, add inputs using `add_input1` and `add_input2`.
-/// The assembler will automatically process the inputs over time, which can be tracked using the `Tick`.
-/// Outputs can be extracted using `take_output` or similar.
-/// If you want to change the recipe, use `change_recipe`, but ensure the assembler is empty first.
+/// To use, first build the assembler using [`Assembler::build`], providing the desired recipe and the required resources.
+/// Then, add inputs using [`add_input1`](Assembler::add_input1) and [`add_input2`](Assembler::add_input2).
+/// The assembler will automatically process the inputs over time, which can be advanced using the [`Tick`].
+/// Outputs can be extracted using [`take_output`](Assembler::take_output) or similar.
+/// If you want to change the recipe, use [`change_recipe`](Assembler::change_recipe), but ensure the assembler is empty first.
 #[derive(Debug)]
 pub struct Assembler<R: AssemblerRecipe> {
     input1_amount: u32,
@@ -215,11 +215,11 @@ impl<R: AssemblerRecipe> Assembler<R> {
 
 /// The furnace is used to smelt ores into base resources.
 ///
-/// To use, first build the furnace using `Furnace::build`, providing the desired recipe and the required resources.
-/// Then, add inputs using `add_input`.
-/// The furnace will automatically process the inputs over time, which can be tracked using the `Tick`.
-/// Outputs can be extracted using `take_output` or similar.
-/// If you want to change the recipe, use `change_recipe`, but ensure the furnace is empty first.
+/// To use, first build the furnace using [`Furnace::build`], providing the desired recipe and the required resources.
+/// Then, add inputs using [`add_input`](Furnace::add_input).
+/// The furnace will automatically process the inputs over time, which can be advanced using the [`Tick`].
+/// Outputs can be extracted using [`take_output`](Furnace::take_output) or similar.
+/// If you want to change the recipe, use [`change_recipe`](Furnace::change_recipe), but ensure the furnace is empty first.
 #[derive(Debug)]
 pub struct Furnace<R: FurnaceRecipe> {
     input_amount: u32,
